1408a70f3637c812e185d459a23789d9ca883a3f
Tasy <sebascontat@gmail.com>
2025-02-07 12:16:07 -0500

    Paguinas del cuento

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+//Seleciona el div
 const image = document.getElementById("imagen-principal");
-const texts = document.querySelectorAll(".texto"); // Corrige la selección
-
+//Selecciona todos los elementos con la clase texto
+const texts = document.querySelectorAll(".texto"); 
+//Itera sobre cada elemento de la clase texto
 texts.forEach(text => {
+  //Agrega un evento mouseenter a cada elemento de la clase texto
   text.addEventListener("mouseenter", () => {
+    //Cambia el src de la imagen principal por el data-img del elemento texto
     image.src = text.getAttribute("data-img");
   });
 });
